4e343f0611663b573669e8dee722bfb64530fab1
Zeeshan Awan <zeeshan.awan@RT-DT-501.rolustech.net>
2016-07-20 18:21:07 +0500

    Arithmetic class implemented with psr2 conventions,php docs generated, performed php unit test

---
+++ zeeshan.awan/Arithmetic/tests/ArithmeticTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace foo;
+
+use PHPUnit_Framework_TestCase as TestCase;
+include_once 'Arithmetic.php';
+
+class ArithmeticTest extends TestCase
+{
+    public function testSum()
+    {
+     $obj = new Arithmetic;
+     $this->assertEquals(5,$obj->add(2, 1));
+    }
+    public function testSub()
+    {
+     $obj = new Arithmetic;
+     $this->assertEquals(2,$obj->subtract(2, 1));
+    }
+    public function testMul()
+    {
+     $obj = new Arithmetic;
+    $this->assertEquals(2,$obj->multiply(2, 1));
+    }
+    public function testDiv()
+    {
+     $obj = new Arithmetic;
+     $this->assertEquals(2,$obj->divide(2, 1));
+
+    }
+}
+
+
