baccaaf656f26739eebcd7641d4d9d097b298b2d
Asad Manzoor <asad.manzoor@RT-DT-502.rolustech.net>
2016-07-20 17:57:20 +0500

    Arithmatic class first commit

---
+++ asad.manzoor/Arithmatic/code.php
@@ -0,0 +1,27 @@
+<?php
+namespace arithmeticMath;
+
+class ArithmeticMath
+{
+  
+    // method declaration
+    public function add($var1, $var2)
+    {
+        return $var1+$var2;
+    }
+
+    public function subtract($var1, $var2)
+    {
+        return $var1-$var2;
+    }
+
+    public function multiply($var1, $var2)
+    {
+        return $var1*$var2;
+    }
+
+    public function divide($var1, $var2)
+    {
+        return $var1/$var2;
+    }
+}
