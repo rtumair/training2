84144e952c156eeafa1ba2abb4706fd3a60a8c56
Zeeshan Awan <zeeshan.awan@RT-DT-501.rolustech.net>
2016-07-20 18:42:00 +0500

    This is my day1 arithmetic test file with php unit testing

---
+++ bilal.ashraf/PHP_UNIT_2/src/Arithmetic.php
@@ -0,0 +1,25 @@
+<?php
+namespace foo;
+
+class Arithmetic
+{
+    public function add($arg1, $arg2)
+    {
+        return $arg1 + $arg2;
+    }
+
+    public function subtract($arg1, $arg2)
+    {
+        return $arg1 - $arg2;
+    }
+
+    public function multiply($arg1, $arg2)
+    {
+        return $arg1 * $arg2;
+    }
+
+    public function divide($arg1, $arg2)
+    {
+        return $arg1 / $arg2;
+    }
+}
