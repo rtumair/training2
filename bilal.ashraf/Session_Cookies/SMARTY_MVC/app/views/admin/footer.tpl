cc38f02c50d63f8cb9713253435922137c86613c
Bilal  Ashraf <bilal_ashraf@RT-LT-264.rolustech.net>
2016-08-03 01:41:17 -0400

    This is my Session handling , Cookies handling and password Encryption with MVC template admin"

---
+++ bilal.ashraf/Session_Cookies/SMARTY_MVC/app/views/admin/footer.tpl
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bilal_ashraf
+ * Date: 7/29/16
+ * Time: 3:44 AM
+ */
---
+++ bilal.ashraf/Session_Cookies/SMARTY_MVC/public/index.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors',true);
+//error_reporting(E_ALL);
+//$val = md5("123");
+//echo $val . "na kar<br>";
+
+require_once '../app/init.php';
+$app = new App;
+
+
